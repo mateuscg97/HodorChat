b1b29ffa4059b6b14cae9a9ea5b1bdfd8a2c61fa
Mateus <mateuscg97@outlook.com>
2016-07-02 15:51:02 -0300

    Criamos a func loginFacebook e getFacebookProfile

---
+++ HodorChat/CONSTANTS.swift
@@ -0,0 +1,12 @@
+//
+//  CONSTANTS.swift
+//  HodorChat
+//
+//  Created by Mateus Da Costa Goncalves on 02/07/16.
+//  Copyright © 2016 Mateus Da Costa Goncalves. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let FIREBASE = Firebase(url: "https://hodorchat.firebaseio.com")
--- HodorChat/ViewController.swift
+++ HodorChat/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginFacebook()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loginFacebook(){
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["public_profile", "email", "user_friends"], handler: {
+            
+            (facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                
+                print("Facebook login was cancelled.")
+            } else {
+                
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                FIREBASE.authWithOAuthProvider("facebook", token: accessToken,
+                    withCompletionBlock: { error, authData in
+                        if error != nil {
+                            
+                            print("Login failed. \(error)")
+                        } else {
+                            
+                            print("Logged in! \(authData)")
+                            self.getFacebookProfile()
+                        }
+                })
+            }
+        })
+    }
+    
+    func getFacebookProfile(){
+        /*id, name, first_name, last_name, age_range, link, gender, locale, timezone,updated_time, verified*/
+        
+        if((FBSDKAccessToken.currentAccessToken()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email, gender"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                if (error == nil){
+                    print(result)
+                }
+            })
+        }
+    }
+    
 }
 
